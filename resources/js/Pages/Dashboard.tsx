3bf3860e6c2112529cc5dc2c853a05e14812017e
ilhamgepe <gepedevelop@gmail.com>
2023-08-07 16:07:31 +0700

    initial commit

---
+++ resources/js/Pages/Dashboard.tsx
@@ -0,0 +1,60 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+import { PageProps } from "@/types";
+import { Carousel } from "@mantine/carousel";
+import { Button, Image, Text } from "@mantine/core";
+import { useRef } from "react";
+
+export default function Dashboard({ auth }: PageProps) {
+    return (
+        <AuthenticatedLayout user={auth.user}>
+            <Head title="Dashboard" />
+
+            <div className="">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg flex flex-col md:flex-row items-center justify-center gap-5 p-3">
+                        <div>
+                            <Carousel
+                                maw={520}
+                                miw={320}
+                                mx="auto"
+                                slideGap={"xl"}
+                                height={300}
+                                loop
+                                draggable
+                            >
+                                <Carousel.Slide>
+                                    <Image
+                                        src={
+                                            "https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+                                        }
+                                    />
+                                </Carousel.Slide>
+                                <Carousel.Slide>
+                                    <Image
+                                        src={
+                                            "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+                                        }
+                                    />
+                                </Carousel.Slide>
+                                <Carousel.Slide>
+                                    <Image
+                                        src={
+                                            "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+                                        }
+                                    />
+                                </Carousel.Slide>
+                            </Carousel>
+                        </div>
+                        <div className="flex items-center flex-col">
+                            <Text size="xl" weight={500}>
+                                Shop now!
+                            </Text>
+                            <Button>Buy</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
---
+++ resources/js/app.tsx
@@ -0,0 +1,30 @@
+import "./bootstrap";
+import "../css/app.css";
+
+import { createRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { MantineProvider } from "@mantine/core";
+
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.tsx`,
+            import.meta.glob("./Pages/**/*.tsx")
+        ),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(
+            <MantineProvider withGlobalStyles withNormalizeCSS>
+                <App {...props} />
+            </MantineProvider>
+        );
+    },
+    progress: {
+        color: "#4B5563",
+    },
+});
