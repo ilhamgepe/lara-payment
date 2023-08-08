172c1fd022d32b6a99b0fe386479de921444fedd
ilhamgepe <gepedevelop@gmail.com>
2023-08-08 17:08:36 +0700

    package tolol. besok lagi dah

--- app/Http/Controllers/gateway/PaypalController.php
+++ app/Http/Controllers/gateway/PaypalController.php
@@ -7,47 +7,101 @@ use App\utilities\payment\PaypalUtils;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Inertia\Inertia;
+use Srmklive\PayPal\Services\PayPal as PayPalClient;
 
 class PaypalController extends Controller
 {
-    use PaypalUtils;
-    public function createOrder()
+
+    public function pakcageCreateOrder()
     {
-        // generate token for paypal
-        $order = $this->__createOrder([
-            [
-                'reference_id' => 'uuid-item-anjing',
-                'amount' => [
-                    'currency_code' => 'USD',
-                    'value' => 30
-                ]
+        // $returnUrl = 'https://73fa-2001-448a-2082-6312-d5ff-73de-a0e3-9784.ngrok-free.app/paypal/success';
+        // $cancelUrl = 'https://73fa-2001-448a-2082-6312-d5ff-73de-a0e3-9784.ngrok-free.app/paypal/failed';
+        $provider = new PayPalClient;
+        $provider->setApiCredentials(config('paypal'));
+        $provider->getAccessToken();
+
+        $response = $provider->setReturnAndCancelUrl('https://73fa-2001-448a-2082-6312-d5ff-73de-a0e3-9784.ngrok-free.app/paypal/success', 'https://73fa-2001-448a-2082-6312-d5ff-73de-a0e3-9784.ngrok-free.app/paypal/failed')->createOrder(json_decode('{
+            "intent": "CAPTURE",
+            "purchase_units": [
+              {
+                "amount": {
+                  "currency_code": "USD",
+                  "value": 100
+                }
+              }
             ]
-        ]);
+        }', true));
 
-        $payerActionHrefs = collect($order['links'])
-            ->where('rel', 'payer-action')
-            ->pluck('href')
-            ->first();
-        return redirect($payerActionHrefs);
-    }
 
-    public function callback(Request $request)
-    {
-        $showOrder = Http::withHeaders([
-            'Authorization' => 'Bearer ' . $this->generateToken(),
-        ])->asJson()->get('https://api-m.sandbox.paypal.com/v2/checkout/orders/' . $request->token);
-
-        if ($showOrder->json()['status'] == 'APPROVED') {
-            session()->flash('success', 'pembayaran berhasil');
-            return Inertia::render('Dashboard');
-        } else {
-            session()->flash('error', 'pembayaran gagal');
-            return Inertia::render('Dashboard');
+        if (isset($response['id'])) {
+            $payerActionHrefs = collect($response['links'])
+                ->where('rel', 'approve')
+                ->pluck('href')
+                ->first();
+            if (!$payerActionHrefs) {
+                session()->flash('error', 'pembayaran gagal, tidak ada id nya');
+                return Inertia::render('Dashboard');
+            }
+            return redirect($payerActionHrefs);
         }
-    }
-    public function failed(Request $request)
-    {
         session()->flash('error', 'pembayaran gagal');
         return Inertia::render('Dashboard');
     }
+
+    public function success(Request $request)
+    {
+        dd($request->all(), 'success');
+    }
+    public function failed(Request $request)
+    {
+        dd($request->all(), 'failed');
+    }
+    public function notify(Request $request)
+    {
+        dd($request->all());
+    }
 }
+// bikin sendiri
+// class PaypalController extends Controller
+// {
+//     // use PaypalUtils;
+//     // public function createOrder()
+//     // {
+//     //     // generate token for paypal
+//     //     $order = $this->__createOrder([
+//     //         [
+//     //             'reference_id' => 'uuid-item-anjing',
+//     //             'amount' => [
+//     //                 'currency_code' => 'USD',
+//     //                 'value' => 30
+//     //             ]
+//     //         ]
+//     //     ]);
+
+//     //     $payerActionHrefs = collect($order['links'])
+//     //         ->where('rel', 'payer-action')
+//     //         ->pluck('href')
+//     //         ->first();
+//     //     return redirect($payerActionHrefs);
+//     // }
+
+//     // public function callback(Request $request)
+//     // {
+//     //     $showOrder = Http::withHeaders([
+//     //         'Authorization' => 'Bearer ' . $this->generateToken(),
+//     //     ])->asJson()->get('https://api-m.sandbox.paypal.com/v2/checkout/orders/' . $request->token);
+
+//     //     if ($showOrder->json()['status'] == 'APPROVED') {
+//     //         session()->flash('success', 'pembayaran berhasil');
+//     //         return Inertia::render('Dashboard');
+//     //     } else {
+//     //         session()->flash('error', 'pembayaran gagal');
+//     //         return Inertia::render('Dashboard');
+//     //     }
+//     // }
+//     // public function failed(Request $request)
+//     // {
+//     //     session()->flash('error', 'pembayaran gagal');
+//     //     return Inertia::render('Dashboard');
+//     // }
+// }
--- routes/paymentgateway/paypal.php
+++ routes/paymentgateway/paypal.php
@@ -4,8 +4,17 @@ use App\Http\Controllers\gateway\PaypalController;
 use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
+// paypal sendiri
+// Route::prefix('paypal')->withoutMiddleware([VerifyCsrfToken::class])->group(function () {
+//     Route::get('createOrder', [PaypalController::class, 'createOrder'])->name('createOrder');
+//     Route::get('callback', [PaypalController::class, 'callback']);
+//     Route::get('failed', [PaypalController::class, 'failed']);
+// });
+
+// paypal package
 Route::prefix('paypal')->withoutMiddleware([VerifyCsrfToken::class])->group(function () {
-    Route::get('createOrder', [PaypalController::class, 'createOrder'])->name('createOrder');
-    Route::get('callback', [PaypalController::class, 'callback']);
-    Route::get('failed', [PaypalController::class, 'failed']);
+    Route::get('createOrder', [PaypalController::class, 'pakcageCreateOrder'])->name('createOrder');
+    Route::get('success', [PaypalController::class, 'success'])->name('paypal.successTransaction');
+    Route::get('failed', [PaypalController::class, 'failed'])->name('paypal.failedTransaction');
+    Route::get('notify', [PaypalController::class, 'notify'])->name('notify');
 });
